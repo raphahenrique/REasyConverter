9f854f8c2f3e349aadaddfedea76ff444042f5cc
raphahenrique <raphael.henrique@live.com>
2023-04-13 23:32:52 -0300

    Added Firebase sdk to project

--- REasy Converter/REasy Converter/AppDelegate.swift
+++ REasy Converter/REasy Converter/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Raphael on 12/03/23.
 //
 
+import FirebaseCore
 import UIKit
 
 @main
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
